9c7ec33a595f3c3e5fe94ecb1188d96208465f5e
Ahmed-programmer-1 <ahmed.programming.556@gmail.com>
2026-08-17 12:27:59 +0300

    إضافة كورس AAA17

---
+++ AAA17.js
@@ -0,0 +1,26 @@
+registerCourse("AAA17", {
+  "img": "AAA17.jpg",
+  "name": "دراغون بول ذد كلاي - الموسم الاول",
+  "description": "دراغون بول ذد كلاي - الموسم الاول",
+  "category": "دراغون بول ذد كلاي",
+  "level": "الموسم الأول",
+  "info": {
+    "overview": "",
+    "studio": "",
+    "originalLanguage": "",
+    "dubLanguage": "",
+    "creator": "",
+    "startDate": "",
+    "endDate": "",
+    "writer": "",
+    "levelsCount": "",
+    "episodesThisLevel": ""
+  },
+  "videos": [
+    {
+      "title": "الحلقة 1",
+      "link": "https://anonmp4.art/v/grUHLulNQmi2ayB",
+      "description": ""
+    }
+  ]
+});
