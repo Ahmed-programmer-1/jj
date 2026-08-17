b727c04378f0bd257e25ef3e51ca22fed229c830
Ahmed-programmer-1 <ahmed.programming.556@gmail.com>
2026-08-17 12:50:51 +0300

    تحديث كورس AAA17

--- AAA17.js
+++ AAA17.js
@@ -19,7 +19,7 @@ registerCourse("AAA17", {
   "videos": [
     {
       "title": "الحلقة 1",
-      "link": "https://anonmp4.art/v/4PJGV7KQQhzfwao",
+      "link": "https://storage.to/yjtaq3LbO",
       "description": ""
     }
   ]
