6d0d1edec87738128ce955f6640fd47851584bd7
Ahmed-programmer-1 <ahmed.programming.556@gmail.com>
2026-09-01 15:57:11 +0300

    تحديث كورس AAA1

--- AAA1.js
+++ AAA1.js
@@ -19,43 +19,8 @@ registerCourse("AAA1", {
   "videos": [
     {
       "title": "الحلقة 1",
-      "link": "https://archive.org/download/ep-1_20260812/EP1.mp4",
+      "link": "https://cdn03.vdocipher.com/media/khdwCaaznDlzD/5d184c56/track.mp4",
       "description": "الحلقة الأولى."
-    },
-    {
-      "title": "الحلقة 2",
-      "link": "https://archive.org/download/ep-2_20260812/EP2.mp4",
-      "description": "الحلقة الثانية."
-    },
-    {
-      "title": "الحلقة 3",
-      "link": "https://archive.org/download/ep-3_20260812/EP3.mp4",
-      "description": "الحلقة الثالثة."
-    },
-    {
-      "title": "الحلقة 4",
-      "link": "https://archive.org/download/ep-4_20260812/EP4.mp4",
-      "description": "الحلقة الرابعة."
-    },
-    {
-      "title": "الحلقة 5",
-      "link": "https://archive.org/download/ep-5_20260812/EP5.mp4",
-      "description": "الحلقة الخامسة."
-    },
-    {
-      "title": "الحلقة 6",
-      "link": "https://archive.org/download/ep-6_20260812/EP6.mp4",
-      "description": "الحلقة السادسة."
-    },
-    {
-      "title": "الحلقة 7",
-      "link": "https://archive.org/download/ep-7_20260812/EP7.mp4",
-      "description": "الحلقة السابعة."
-    },
-    {
-      "title": "الحلقة 8",
-      "link": "https://archive.org/download/ep-8_20260812/EP8.mp4",
-      "description": "الحلقة الثامنة."
     },
     {
       "title": "الحلقة 9",
