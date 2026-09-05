814bf977f9d6517f81307144756df9a40b5c5fef
Ahmed-programmer-1 <ahmed.programming.556@gmail.com>
2026-09-05 15:18:38 +0300

    إضافة كورس AAA33

---
+++ AAA33.js
@@ -0,0 +1,26 @@
+registerCourse("AAA33", {
+  "img": "",
+  "name": "الدرس الاول الخبير",
+  "description": "الدرس الاول الخبير",
+  "category": "",
+  "level": "",
+  "info": {
+    "overview": "",
+    "studio": "",
+    "originalLanguage": "",
+    "dubLanguage": "",
+    "creator": "",
+    "startDate": "",
+    "endDate": "",
+    "writer": "",
+    "levelsCount": "",
+    "episodesThisLevel": ""
+  },
+  "videos": [
+    {
+      "title": "الحلقة 1",
+      "link": "https://www.youtube.com/embed/iWaYJjadAk8?autoplay=1&controls=0&modestbranding=1&rel=0&showinfo=0&loop=0&fs=0&hl=ar&enablejsapi=1&origin=https%3A%2F%2Ftheexpert.online&widgetid=1&forigin=https%3A%2F%2Ftheexpert.online%2Fcourse%2Flearning%2Fmonth-1-bakalorya&aoriginsup=1&gporigin=https%3A%2F%2Ftheexpert.online%2Fpanel%2Fwebinars%2Fpurchases&vf=5",
+      "description": ""
+    }
+  ]
+});
